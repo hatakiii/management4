15e867452ac2d739a0401b41cb82e536d8a9d99d
hatakiii <25lp5598@nest.edu.mn>
2026-01-13 21:15:53 +0800

    get and post req

--- app/api/attendance/route.ts
+++ app/api/attendance/route.ts
@@ -7,8 +7,35 @@ export async function GET() {
     const attendance = await prisma.attendance.findMany();
     return NextResponse.json(attendance);
   } catch (error) {
+    console.error("api/attendance/route.ts error:", error);
     return NextResponse.json(
       { message: "Failed to fetch attendance" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const attendance = await prisma.attendance.create({
+      data: {
+        userId: Number(body.userId),
+
+        date: new Date(body.date),
+        clockIn: new Date(body.clockIn),
+        clockOut: body.clockOut ? new Date(body.clockOut) : null,
+
+        updatedAt: new Date(),
+      },
+    });
+
+    return NextResponse.json(attendance, { status: 201 });
+  } catch (error) {
+    console.error("api/attendance/route.ts error:", error);
+    return NextResponse.json(
+      { message: "Failed to post attendance" },
       { status: 500 }
     );
   }
